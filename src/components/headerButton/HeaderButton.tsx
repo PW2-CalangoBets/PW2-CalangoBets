d9eccea5b56397244242dbabe849b1aec180acbe
Osoapy <oosoapy@gmail.com>
2025-07-08 12:35:17 -0300

    fix: relying on resizeble design

--- src/components/headerButton/HeaderButton.tsx
+++ src/components/headerButton/HeaderButton.tsx
@@ -1,5 +1,20 @@
-const headerButton = () => {
-    return (
-        <div>HeaderButton</div>
-    )
-}
+import './headerButton.scss';
+
+type HeaderButtonProps = {
+  label: string;
+  isSpecial: boolean;
+  onClick: () => void;
+};
+
+const HeaderButton = ({ label, isSpecial, onClick }: HeaderButtonProps) => {
+  return (
+    <button
+      className={`header-button${isSpecial ? ' special' : ''}`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default HeaderButton;
--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -2,6 +2,7 @@ import './landingPage.scss';
 import { useNavigate } from 'react-router-dom';
 import StandardHeader from '../../components/standardHeader/StandardHeader';
 import HeaderLink from '../../components/headerLinks/HeaderLinks';
+import HeaderButton from '../../components/headerButton/HeaderButton';
 
 const LandingPage = () => {
     const navigate = useNavigate();
@@ -16,7 +17,8 @@ const LandingPage = () => {
                 <HeaderLink linkName="Jogos" onClick={() => handleClick('jogos')}/>
                 <HeaderLink linkName="Sobre" onClick={() => handleClick('sobre')}/>
                 <HeaderLink linkName="Equipe Calango" onClick={() => handleClick('equipe')}/>
-                
+                <HeaderButton label="Login" isSpecial={false} onClick={() => handleClick('login')}></HeaderButton>
+                <HeaderButton label="Cadastre-se" isSpecial={true} onClick={() => handleClick('signup')}></HeaderButton>
             </StandardHeader>
             <div className="landingPage-container">
             </div>
