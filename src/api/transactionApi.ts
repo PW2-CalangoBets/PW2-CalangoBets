4800c3384c8614139760bd1c9a616043958aab1e
Daniel Lima <daniellimaf14@gmail.com>
2025-08-26 10:59:32 -0300

    add: criando axios para transacao

---
+++ src/api/transactionApi.ts
@@ -0,0 +1,29 @@
+import api from "./axios";
+
+export type TransactionRequest = {
+    operation: "DEPOSIT" | "WITHDRAW";
+    value: number;
+    accountCdb: number;
+    date: string;
+};
+
+export type TransactionResponse = {
+    id: string;
+    operation: string;
+    value: number;
+    accountCdb: number;
+    date: string;
+};
+
+export const createTransactionApi = async (
+    transaction: TransactionRequest
+): Promise<TransactionResponse> => {
+    const { data } = await api.post<TransactionResponse>("/api/transaction", transaction);
+    return data;
+};
+
+export const getTransactionsApi = async (): Promise<TransactionResponse[]> => {
+    const { data } = await api.get("/api/transaction");
+    // Ajuste para retornar um array se o backend retornar um objeto paginado
+    return data.content || data;
+};
