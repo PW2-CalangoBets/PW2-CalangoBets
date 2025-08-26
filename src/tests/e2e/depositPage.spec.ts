9d0b27be4a5213bd8682b496fad2fa9858ec6cb3
Kahe Mikáyas <kahe.gohan@gmail.com>
2025-08-26 20:09:46 -0300

    fix: add login in tests

--- src/tests/e2e/depositPage.spec.ts
+++ src/tests/e2e/depositPage.spec.ts
@@ -1,4 +1,28 @@
 import { test, expect } from '@playwright/test';
+import { getUserInfoApi } from '../../api/userApi';
+test.beforeEach(async ({ page }) => {
+  // Vai para tela de login
+  await page.goto("http://localhost:5173/#/signUp");
+
+  // Preenche credenciais
+  await page.fill('input[id="name"]', "UsuarioTeste");
+  await page.fill('input[id="email"]', "teste@gmail.com");
+  await page.fill('input[id="password"]', "123456");
+  await page.fill('input[id="confirmPassword"]', "123456");
+
+  // Submete login
+  await page.click('button[type="submit"]');
+
+  await page.goto('http://localhost:5173/#/login'); 
+  await page.fill('input[id="email"]', "teste@gmail.com");
+  await page.fill('input[id="password"]', "123456");
+
+  // Submete login
+ await page.click('button[type="submit"]');
+
+  // Aguarda que redirecione / mostre dashboard
+  await expect(page.getByText('Sua sorte começa aqui!')).toBeVisible();
+});
 
 test('Deve abrir o modal de Depósito e fazer um depósito', async ({ page }) => {
   await page.goto('http://localhost:5173/#/deposit'); 
@@ -16,7 +40,7 @@ test('Deve abrir o modal de Saque e tentar sacar valor inválido', async ({ page
   await page.click('text=Sacar');
 
   await expect(page.getByRole('heading', { name: 'Saque' })).toBeVisible();
-  await page.fill('input[type="number"]', '1000');
+  await page.fill('input[type="number"]', '1000000');
   page.once('dialog', async dialog => {
     expect(dialog.message()).toContain('insuficiente');
     await dialog.dismiss();
@@ -28,24 +52,25 @@ test('Deve abrir o modal de Saque e tentar sacar valor inválido', async ({ page
 
 
 test('Deve fazer um deposito e logo após tentar sacar parte desse deposito', async ({ page }) => {
-  await page.goto('http://localhost:5173/#/deposit'); 
-  await page.click('text=Depositar');
+  await page.goto('http://localhost:5173/#/deposit');
+  
+
+  
+  await page.click('button:has-text("Depositar")');
   await expect(page.getByRole('heading', { name: 'Depósito' })).toBeVisible();
+  
+
   await page.fill('input[type="number"]', '100');
-  await page.click('text=Realizar o depósito');
-  await expect(page.getByText('100 🪙')).toBeVisible();
+  const saldoAntesText = await page.locator(".actual-Money-div").innerText();
+  const saldoAntes = parseInt(saldoAntesText.replace(" 🪙", ""))
+  await page.click('button:has-text("Realizar o depósito")');
 
-   await page.goto('http://localhost:5173/#/deposit');
+  await expect(page.locator(".actual-Money-div")).toHaveText((saldoAntes + 100) + " 🪙");
 
-  await page.click('text=Sacar');
-
+  await page.click('button:has-text("Sacar")');
   await expect(page.getByRole('heading', { name: 'Saque' })).toBeVisible();
   await page.fill('input[type="number"]', '50');
+  await page.click('button:has-text("Realizar o saque")');
 
-  await page.click('text=Realizar o saque');
-
-  await expect(page.getByText('50 🪙')).toBeVisible();
-
-
-
+  await expect(page.locator(".actual-Money-div")).toHaveText((saldoAntes + 50) + " 🪙");
 });
